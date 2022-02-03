fd0c65d5b658c53ca4c6e2524fc6fd1f1f616211
ReactNativ <51997297+ReactNativ@users.noreply.github.com>
2022-02-03 11:48:18 +0330

    Update App.js

--- example/App.js
+++ example/App.js
@@ -1,12 +1,76 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, Image, View, TouchableOpacity, Switch } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { copilot, walkthroughable, CopilotStep } from 'react-native-copilot';
 
 const WalkthroughableText = walkthroughable(Text);
 const WalkthroughableImage = walkthroughable(Image);
+const WalkthroughableView = walkthroughable(View);
+
+function App({ start, copilotEvents }) {
+
+  const [secondStepActive, setSecondStepActive] = useState(true);
+
+
+  useEffect(() => {
+    copilotEvents.on('stepChange', handleStepChange);
+    start();
+  }, []);
+
+
+  const handleStepChange = (step) => {
+    console.log(`Current step is: ${step.name}`);
+  }
+
+  return (
+    <View style={styles.container}>
+
+
+      <CopilotStep text="Hey! This is the first step of the tour!" order={1} name="openApp">
+        <WalkthroughableView style={styles.titleView}>
+          <Text style={styles.title}>
+            {'Welcome to the demo of\n"React Native Copilot"'}
+          </Text>
+        </WalkthroughableView>
+      </CopilotStep>
+
+      <View style={styles.middleView}>
+
+        <CopilotStep
+          text="Here goes your profile picture!" order={2} name="secondText">
+          <WalkthroughableImage
+            source={{ uri: 'https://pbs.twimg.com/profile_images/527584017189982208/l3wwN-l-_400x400.jpeg' }}
+            style={styles.profilePhoto}
+          />
+        </CopilotStep>
+
+        <View style={styles.activeSwitchContainer}>
+          <Text>Profile photo step activated?</Text>
+          <View style={{ flexGrow: 1 }} />
+          <Switch
+            onValueChange={secondStepActive => setSecondStepActive(secondStepActive)}
+            value={secondStepActive}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => start()}>
+          <Text style={styles.buttonText}>START THE TUTORIAL!</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <CopilotStep text="Here is an item in the corner of the screen." order={3} name="thirdText">
+          <WalkthroughableText style={styles.buttonTab}>
+            <Ionicons name="home" size={30} color="#888" />
+          </WalkthroughableText>
+        </CopilotStep>
+
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -14,6 +78,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     paddingTop: 40,
+  },
+  titleView: {
+    justifyContent: 'center',
+    height: 80,
+    backgroundColor: '#aaa',
+    marginTop: 20
   },
   title: {
     fontSize: 24,
@@ -38,9 +108,15 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
   },
+  buttonTab: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    bottom: 0
+  },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    // flexDirection: 'row',
+    //justifyContent: 'space-between',
+    bottom: 0
   },
   tabItem: {
     flex: 1,
@@ -48,80 +124,23 @@ const styles = StyleSheet.create({
   },
   activeSwitchContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
+    // justifyContent: 'space-between',
+    // marginBottom: 20,
     alignItems: 'center',
     paddingHorizontal: 40,
   },
 });
 
-class App extends Component {
-  static propTypes = {
-    start: PropTypes.func.isRequired,
-    copilotEvents: PropTypes.shape({
-      on: PropTypes.func.isRequired,
-    }).isRequired,
-  };
 
-  state = {
-    secondStepActive: true,
-  };
-
-  componentDidMount() {
-    this.props.copilotEvents.on('stepChange', this.handleStepChange);
-    this.props.start();
-  }
-
-  handleStepChange = (step) => {
-    console.log(`Current step is: ${step.name}`);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <CopilotStep text="Hey! This is the first step of the tour!" order={1} name="openApp">
-          <WalkthroughableText style={styles.title}>
-            {'Welcome to the demo of\n"React Native Copilot"'}
-          </WalkthroughableText>
-        </CopilotStep>
-        <View style={styles.middleView}>
-          <CopilotStep active={this.state.secondStepActive} text="Here goes your profile picture!" order={2} name="secondText">
-            <WalkthroughableImage
-              source={{ uri: 'https://pbs.twimg.com/profile_images/527584017189982208/l3wwN-l-_400x400.jpeg' }}
-              style={styles.profilePhoto}
-            />
-          </CopilotStep>
-          <View style={styles.activeSwitchContainer}>
-            <Text>Profile photo step activated?</Text>
-            <View style={{ flexGrow: 1 }} />
-            <Switch
-              onValueChange={secondStepActive => this.setState({ secondStepActive })}
-              value={this.state.secondStepActive}
-            />
-          </View>
-
-          <TouchableOpacity style={styles.button} onPress={() => this.props.start()}>
-            <Text style={styles.buttonText}>START THE TUTORIAL!</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <CopilotStep text="Here is an item in the corner of the screen." order={3} name="thirdText">
-            <WalkthroughableText style={styles.tabItem}>
-              <Ionicons name="ios-contact" size={40} color="#888" />
-            </WalkthroughableText>
-          </CopilotStep>
-
-          <Ionicons style={styles.tabItem} name="ios-game-controller-b" size={40} color="#888" />
-          <Ionicons style={styles.tabItem} name="ios-globe" size={40} color="#888" />
-          <Ionicons style={styles.tabItem} name="ios-navigate-outline" size={40} color="#888" />
-          <Ionicons style={styles.tabItem} name="ios-rainy" size={40} color="#888" />
-        </View>
-      </View>
-    );
-  }
-}
+App.propTypes = {
+  start: PropTypes.func.isRequired,
+  copilotEvents: PropTypes.shape({
+    on: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default copilot({
+  verticalOffset: 25,
   animated: true, // Can be true or false
   overlay: 'svg', // Can be either view or svg
 })(App);
